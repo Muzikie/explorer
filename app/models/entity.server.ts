196e66f9ea5461bac102bab1b24bd717a048a8ac
Ali Haghighatkhah <ali@muzikie.com>
2023-06-11 21:42:28 +0200

    Add pagintion to server endpoints

--- app/models/entity.server.ts
+++ app/models/entity.server.ts
@@ -1,22 +1,26 @@
-import type{ Collection, Profile, Audio, EndpointParams } from '~/configs/types';
+import type{ Collection, Profile, Audio, Transaction, EndpointParams, AwaitedEndpointResult } from '~/configs/types';
 import { API_URLS, API_VERSION } from '~/configs/api';
+import { removeNullValues } from '~/helpers/helpers';
 
-const get = (url: string) => fetch(url).then((res) => res.json()).then(res => res.data);
+const getList = (entity: string, params: EndpointParams) => {
+  const validatedParams = removeNullValues(params);
+  const search = new URLSearchParams(validatedParams);
+  const queryString = search.toString()
+  return fetch(`${API_URLS.STREAMER}/api/${API_VERSION}/${entity}?${queryString}`).then((res) => res.json());
+};
 
-export async function getCollections({ params }: EndpointParams): Promise<Array<Collection>> {
-  const queryParams = new URLSearchParams(params);
-  console.log(queryParams);
-  return get(`${API_URLS.STREAMER}/api/${API_VERSION}/collections`).then(res => res);
+export async function getCollections({ params }: { params: EndpointParams }): AwaitedEndpointResult<Array<Collection>> {
+  return getList('collections', params);
 }
 
-export async function getAudios({ params }: EndpointParams): Promise<Array<Audio>> {
-  const queryParams = new URLSearchParams(params);
-  console.log(queryParams);
-  return get(`${API_URLS.STREAMER}/api/${API_VERSION}/audios`);
+export async function getAudios({ params }: { params: EndpointParams }): AwaitedEndpointResult<Array<Audio>> {
+  return getList('audios', params);
 }
 
-export async function getProfiles({ params }: EndpointParams): Promise<Array<Profile>> {
-  const queryParams = new URLSearchParams(params);
-  console.log(queryParams);
-  return get(`${API_URLS.STREAMER}/api/${API_VERSION}/profiles`);
+export async function getProfiles({ params }: { params: EndpointParams }): AwaitedEndpointResult<Array<Profile>> {
+  return getList('profiles', params);
+}
+
+export async function getTransactions({ params }: { params: EndpointParams }): AwaitedEndpointResult<Array<Transaction>> {
+  return getList('transactions', params);
 }
