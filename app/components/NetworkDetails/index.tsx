9d350b51b915e94f6370b755baf01b72240a05ae
Ali Haghighatkhah <ali@muzikie.com>
2023-06-11 09:59:38 +0200

    Add network details with mock info

---
+++ app/components/NetworkDetails/index.tsx
@@ -0,0 +1,11 @@
+const NetworkDetails = () => (
+	<div>
+		<span>Network: </span>
+		<strong>Betanet</strong>
+
+		<span>Latest block: </span>
+		<strong>12121212</strong>
+	</div>
+);
+
+export default NetworkDetails
