d24bcd0471f3a956885b3869c1731d090171bb7a
Ali Haghighatkhah <ali@muzikie.com>
2023-06-16 11:26:40 +0200

    Add background color

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-red-100">
         <MainHeader />
         <Outlet />
         <ScrollRestoration />
