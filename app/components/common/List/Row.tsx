e4f4588921a5c8b143de91e48d39c138bf297fc3
Ali Haghighatkhah <ali@muzikie.com>
2023-06-18 09:53:44 +0200

    Link items in lists to the details page

--- app/components/common/List/Row.tsx
+++ app/components/common/List/Row.tsx
@@ -1,3 +1,7 @@
+import { Link } from '@remix-run/react';
+
+import type { Entity } from '~/configs';
+import { getEntityId } from '~/helpers/helpers';
 import type { RowProps, ColumnProps } from './types';
 
 const Column = ({ data, className }: ColumnProps) => (
@@ -6,10 +10,14 @@ const Column = ({ data, className }: ColumnProps) => (
   </div>
 );
 
-const Row = <T,>({ data, itemConfig }: RowProps<T>) => {
+const Row = <T,>({ data, itemConfig, entity }: RowProps<T>) => {
   const configs = itemConfig(data);
+  const id = getEntityId(data as Entity, entity);
   return (
-    <div className={`grid ${configs.gridClassName} gap-4 justify-between gap-x-6 py-5 text-base font-light hover:font-semibold mb-1 rounded-3xl cursor-pointer`}>
+    <Link
+      to={`/${entity}s/${id}`}
+      className={`grid ${configs.gridClassName} gap-4 justify-between gap-x-6 py-5 text-base font-light hover:font-semibold mb-1 rounded-3xl cursor-pointer`}
+    >
       {
         configs.items.map((config, index) => (
           <Column
@@ -19,7 +27,7 @@ const Row = <T,>({ data, itemConfig }: RowProps<T>) => {
           />
         ))
       }
-    </div>
+    </Link>
   );
 };
 
